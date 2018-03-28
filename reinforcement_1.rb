bc72ee83cad43aba138071fb8a9e311bd9d9564a
GORDON SOROKOLIT <gordon.sorokolit@gmail.com>
2018-03-28 17:10:17 -0400

    updated guest list

--- reinforcement_1.rb
+++ reinforcement_1.rb
@@ -1,7 +1,48 @@
-number_list = [1,3,8,2,5,9,18,12,17]
+# ex 1
+#
+# number_list = [1,3,8,2,5,9,18,12,17]
+#
+# number_list.each do |number|
+#   if number%2 !=0
+#     p number
+#   end
+# end
 
-number_list.each do |number|
-  if number%2 !=0
-    p number
+# ex 2 alternate
+
+guest_list = ["Rod", "Todd", "General Zod"]
+
+p "Please enter your name to confirm guest status"
+user = gets.chomp
+
+def search(guest_list,user)
+guest_list.each do |name|
+  if user == name
+    return "Welcome back #{user}"
   end
+    return "I'm sorry, you're not on the list"
 end
+end
+
+
+p search(guest_list,user)
+
+# ex 2
+
+# guest_list.include?(user)
+
+# if guest_list.include?(user)
+#   p "So nice to see you again " + user
+# else
+#   p "I'm sorry, you're not on the list"
+#
+# end
+
+
+#     if name = user
+#     else
+#
+# end
+
+#
+#
